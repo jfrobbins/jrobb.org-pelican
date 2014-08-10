fa8a33bdab5ff9bb6860e187c5bcd9e29407cd92
jrobb <jfrobbins@gmail.com>
2014-08-10 16:34:38 -0400

    fixed some atom/rss feed stuff, there was a null value

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 
 AUTHOR = u'Jon Robbins'
 SITENAME = u'jrobb.org'
-SITEURL = 'http://jrobb.org/'
+SITEURL = 'http://jrobb.org'
 
 THEME = '../pelican-themes/bootstrap2'
 #THEME = '../pelican-themes/foundation-default-colours'
@@ -16,18 +16,28 @@ TIMEZONE = 'America/New_York'
 DEFAULT_LANG = u'en'
 
 # Feed generation is usually not desired when developing
-FEED_ALL_ATOM = None
-CATEGORY_FEED_ATOM = None
+FEED = 'feeds/all.atom.xml'
+FEED_DOMAIN = SITEURL
 TRANSLATION_FEED_ATOM = None
+FEED_ALL_ATOM = None
+#FEED_ALL_ATOM = 'feeds/all.atom.xml'
+#CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
+#AUTHOR_FEED_ATOM = 'feeds/%s.atom.xml'
+
+# Feeds 
+FEEDS =  (('All posts', 'feeds/all.atom.xml'),
+          ('Category', 'feeds/category'),
+         )
 
 # Blogroll
 LINKS = (('my GMG', 'https://media.jrobb.org/'),
          ('hastebin', 'http://bin.jrobb.org/'),
          ('pump.io', 'https://io.jrobb.org/'),
-         )
+        )
 
 # Social widget
-SOCIAL = (('pump.io', 'https://io.jrobb.org'),
+SOCIAL = (
+          ('pump.io', 'https://io.jrobb.org'),
           ('@jrobb', 'https://quitter.se/jrobb'),
           ('g+', 'https://plus.google.com/+JonathanRobbinsNC/'),
           ('@jrobbnc', 'https://twitter.com/jrobbnc'),
